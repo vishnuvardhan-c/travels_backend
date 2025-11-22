b3682f8c8acb14c2944ec0966a84b2e09278205e
vishnuvardhan-c <chityalavishnuvardhan28@gmail.com>
2025-11-22 11:22:26 +0530

    updated req

--- travels/travels/wsgi.py
+++ travels/travels/wsgi.py
@@ -9,8 +9,8 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 
 import os
 
-from django.core.wsgi import get_wsgi_application
+from django.core.wsgi import get_wsgi_application # type: ignore
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'travels.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'travels.travels.settings')
 
 application = get_wsgi_application()
